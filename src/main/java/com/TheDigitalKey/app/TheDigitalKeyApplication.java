63811936a3ef9a34735cae678229f56813f75144
Steven07TV <brandon.steven1259@hotmail.com>
2024-02-22 15:54:03 -0500

    Se agrega el Proyecto

---
+++ src/main/java/com/TheDigitalKey/app/TheDigitalKeyApplication.java
@@ -0,0 +1,13 @@
+package com.TheDigitalKey.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheDigitalKeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheDigitalKeyApplication.class, args);
+	}
+
+}
